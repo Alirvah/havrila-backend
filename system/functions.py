a7696754960ec1b958f85dcbaff4263d3c5d8421
Patrik Havrila <havrilap@gmail.com>
2022-01-27 22:09:16 +0100

    valheim auto turn off

--- system/functions.py
+++ system/functions.py
@@ -2,6 +2,9 @@ from system import models as systemModels
 import datetime
 from django.utils.timezone import make_aware
 import boto3
+import requests
+import json
+from django.conf import settings
 
 def archiveWorld():
     s3 = boto3.resource('s3')
@@ -10,31 +13,23 @@ def archiveWorld():
     s3.meta.client.copy({'Bucket':'minecraft.bak','Key':newestFile},'minecraft-world-download','test.tar.gz')
 
 
-INSTANCE_ID = 'i-07d6d549d3b5cdb92'
-EC2 = 'ec2'
-
-def stopServer():
-    ec2 = boto3.resource(EC2)
-    instance = ec2.Instance(INSTANCE_ID)
-    instance.stop()
-
-
-def startServer():
-    ec2 = boto3.resource(EC2)
-    instance = ec2.Instance(INSTANCE_ID)
-    instance.start()
-
-
-def getServerState():
-    ec2 = boto3.resource(EC2)
-    instance = ec2.Instance(INSTANCE_ID)
-    return instance.state['Name']
-
-
 def stopIfEmpty():
+    ec2 = boto3.resource('ec2')
+    instance = ec2.Instance(settings.INSTANCE['minecraft'])
     online = systemModels.Online.objects.all().first()
     now = datetime.datetime.now()
     oneHourOld = make_aware(now-datetime.timedelta(hours=1))
     if online.created_at < oneHourOld:
-        if getServerState() == 'running':
-            stopServer()
+        if instance.state['Name'] == 'running':
+            instance.stop()
+
+
+def stopValheimIfEmpty():
+    ec2 = boto3.resource('ec2')
+    instance = ec2.Instance(settings.INSTANCE['valheim'])
+    url = f'https://api.steampowered.com/IGameServersService/GetServerList/v1/?key={settings.STEAM_KEY}&filter=%5Caddr%5C{instance.public_ip_address}'
+    response = json.loads(requests.get(url).text)
+    if instance.state['Name'] == 'running':
+        if response and 'servers' in response['response']:
+            if response['response']['servers'][0]['players'] == 0:
+               instance.stop()
